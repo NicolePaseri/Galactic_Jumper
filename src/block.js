05375dba40d261a02f7b16c874ffc7803840034f
madelinedasilvadias <114189247+madelinedasilvadias@users.noreply.github.com>
2024-04-20 14:07:17 +0200

    Adding Alien

--- src/block.js
+++ src/block.js
@@ -13,32 +13,49 @@ function block() {
     this.powerup;
     this.type;
     this.obstacle;
+    this.particule;
+    this.alien;
     this.direction = "right";
     this.moveTime = 10;
 
     // Plateformes qui se cassent et plateformes qui bougent
-    this.draw = function() {
-        if (this.type === "break") {
-            ctx.fillStyle = "#876537";
-        } else if (this.type === "sideways") {
-            ctx.fillStyle = "#14a7c8";
-        } else {
-            ctx.fillStyle = "#FFFFFF"; // Plateforme blanche retirée d'ici
-        }
-
-        if (this.obstacle !== 0) {
-            obstacleFunctions[this.obstacle].draw(this.x, this.y);
-        } else if (this.particule !== 0) {
-                particuleFunctions[this.particule].draw(this.x, this.y);
-            } else if (this.powerup === "cloud") {
-                ctx.drawImage(cloudImage, this.x + 25, this.y - 20, 90, 90); // Ajustez la taille ici
-                } else if (this.powerup === "rocket") {
-                    ctx.drawImage(rocketImage, this.x + 20, this.y - 100, 100, 100); // Ajustez la taille ici
-                    ctx.fillRect(this.x, this.y, this.width, this.height);
-                        } else {
-                            ctx.fillRect(this.x, this.y, this.width, this.height);
-                        }
+this.draw = function() {
+    if (this.type === "break") {
+        ctx.fillStyle = "#876537";
+    } else if (this.type === "sideways") {
+        ctx.fillStyle = "#14a7c8";
+    } else {
+        ctx.fillStyle = "#FFFFFF"; // Plateforme blanche retirée d'ici
     }
+
+    // Dessiner l'obstacle si présent
+    if (this.obstacle !== 0) {
+        obstacleFunctions[this.obstacle].draw(this.x, this.y);
+    } 
+    // Dessiner la particule si présente
+    else if (this.particule !== 0) {
+        particuleFunctions[this.particule].draw(this.x, this.y);
+    } 
+    // Dessiner le power-up cloud si présent
+    else if (this.powerup === "cloud") {
+        ctx.drawImage(cloudImage, this.x + 25, this.y - 20, 90, 90); // Ajustez la taille ici
+    } 
+    // Dessiner le power-up rocket si présent
+    else if (this.powerup === "rocket") {
+        ctx.drawImage(rocketImage, this.x + 20, this.y - 100, 100, 100); // Ajustez la taille ici
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    } 
+    // Dessiner l'alien si présent
+    else if (this.alien !== 0) {
+        alienFunctions[this.alien].draw(this.x, this.y); // Assurez-vous d'avoir les fonctions appropriées pour dessiner les aliens
+    } 
+    // Si aucun des cas ci-dessus, dessiner simplement un rectangle
+    else {
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+
     this.update = function() {
         if (this.type === "sideways") {
             if (this.x >= screenWidth - this.width) {
