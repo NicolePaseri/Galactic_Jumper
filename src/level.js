7f0e1829db7ace806b4607c541f5d0362b9285e3
NicolePaseri <117440782+NicolePaseri@users.noreply.github.com>
2024-05-14 20:56:38 +0200

    logo volume

--- src/level.js
+++ src/level.js
@@ -1,3 +1,5 @@
+var temporaryImage = "Sprites/AlienBackGround.png"; // Chemin de votre image temporaire
+
 function updateLevel(score) {
     if (score < 128133) {
         level = 1;
@@ -6,8 +8,14 @@ function updateLevel(score) {
         
     } else if (score < 256267) {
         level = 2;
-        backgroundImage = backgroundImageLevel2;
-        obstacle.img.src = "Sprites/GalacticDangers/Satélite-transformed.png";
+        // Affichage de l'image temporaire avant le niveau 2
+        backgroundImage = backgroundImageLevel2; // Assurez-vous de ne pas changer l'arrière-plan
+        obstacle.img.src = temporaryImage; // Affichez l'image temporaire
+        
+        // Réinitialisez l'image de l'obstacle après 5 secondes
+        setTimeout(function() {
+            obstacle.img.src = "Sprites/GalacticDangers/Satélite-transformed.png";
+        }, 5000); // 5000 millisecondes = 5 secondes
         
     } else if (score < 384400 ){
         level = 3;
