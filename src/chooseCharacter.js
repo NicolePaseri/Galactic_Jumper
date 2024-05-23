d182fabdcf927531ddbf722d78299e5b04a11525
kevinferreira2 <kevin.ferreira2@hes-so.ch>
2024-05-23 13:44:40 +0200

    Fix

--- src/chooseCharacter.js
+++ src/chooseCharacter.js
@@ -1,29 +1,50 @@
-
 function showCharacter() {
     var characterDiv = document.createElement("div");
-    characterDiv.style.backgroundImage = 'url("Sprites/PersonnageBackground.png")';
-    characterDiv.style.width = "1700px";
-    characterDiv.style.height = "900px";
-    characterDiv.style.backgroundSize = "cover";
     characterDiv.style.position = "absolute";
     characterDiv.style.top = "0";
     characterDiv.style.left = "0";
+    characterDiv.style.width = "100vw";
+    characterDiv.style.height = "100vh";
     characterDiv.style.zIndex = "9999";
+    characterDiv.style.overflow = "hidden"; // Assurez-vous que le contenu supplémentaire est masqué
+    characterDiv.style.display = "flex";
+    characterDiv.style.flexDirection = "column";
+    characterDiv.style.alignItems = "center";
+    characterDiv.style.justifyContent = "center";
+
+    // Ajout de la vidéo de fond
+    var videoBackground = document.createElement('video');
+    videoBackground.src = 'Sprites/Character.mp4'; // Remplacez par le chemin de votre vidéo
+    videoBackground.autoplay = true;
+    videoBackground.loop = true;
+    videoBackground.muted = true;
+    videoBackground.style.position = 'absolute';
+    videoBackground.style.top = '50%';
+    videoBackground.style.left = '50%';
+    videoBackground.style.transform = 'translate(-50%, -50%)';
+    videoBackground.style.width = '100%';
+    videoBackground.style.height = '100%';
+    videoBackground.style.objectFit = 'cover'; // Assurez-vous que la vidéo couvre l'ensemble du conteneur
+    videoBackground.style.zIndex = '-1'; // Met la vidéo en arrière-plan
+
+    characterDiv.appendChild(videoBackground); // Ajoute la vidéo au conteneur
 
     var charactersContainer = document.createElement("div");
     charactersContainer.style.display = "flex";
     charactersContainer.style.justifyContent = "center";
     charactersContainer.style.flexDirection = "row";
-    charactersContainer.style.padding = "500px";
+    charactersContainer.style.flexWrap = "wrap";
+    charactersContainer.style.padding = "20px";
+    charactersContainer.style.zIndex = '1'; // Assurez-vous que le conteneur de personnages est au-dessus de la vidéo
+    charactersContainer.style.position = 'relative'; // Position relative pour assurer la visibilité
 
-    var characters = ["Sprites/Personnage/Alien.png","Sprites/Personnage/Magicien.png", "Sprites/Personnage/MonsterOrange.png","Sprites/Personnage/Astronaute.png", "Sprites/Personnage/Monster.png"  ];
+    var characters = ["Sprites/Personnage/Alien.png","Sprites/Personnage/Magicien.png", "Sprites/Personnage/MonsterOrange.png","Sprites/Personnage/Astronaute.png", "Sprites/Personnage/Monster.png"];
     characters.forEach(function (character) {
-        
         var characterImage = document.createElement("img");
         characterImage.src = character;
         characterImage.style.width = "100px";
         characterImage.style.height = "100px";
-        characterImage.style.marginBottom = "10px";
+        characterImage.style.margin = "10px";
         characterImage.draggable = true;
         characterImage.addEventListener("dragstart", function (event) {
             event.dataTransfer.setData("text/plain", character);
@@ -36,10 +57,8 @@ function showCharacter() {
     dropZoneDiv.style.height = "200px";
     dropZoneDiv.style.border = "2px dashed black";
     dropZoneDiv.style.margin = "20px";
-    dropZoneDiv.style.position = "absolute";
-    dropZoneDiv.style.top = "50%";
-    dropZoneDiv.style.right = "20px"; // Déplacer le carré vers la droite de 20 pixels
-    dropZoneDiv.style.transform = "translateY(-50%)"; // Centrer le carré verticalement
+    dropZoneDiv.style.position = "relative";
+    dropZoneDiv.style.zIndex = '1'; // Assurez-vous que la zone de dépôt est au-dessus de la vidéo
 
     dropZoneDiv.addEventListener("dragover", function (event) {
         event.preventDefault();
@@ -70,43 +89,41 @@ function showCharacter() {
             alert("You cannot upload a file.");
         }
     });
-    
 
     var buttonsContainerDiv = document.createElement("div"); // Container pour les boutons
     buttonsContainerDiv.style.position = "absolute";
-    buttonsContainerDiv.style.top = "700px"; // Position en haut de la page
+    buttonsContainerDiv.style.bottom = "20px"; // Position en bas de la page
     buttonsContainerDiv.style.left = "50%";
     buttonsContainerDiv.style.transform = "translateX(-50%)";
     buttonsContainerDiv.style.display = "flex";
+    buttonsContainerDiv.style.zIndex = '1'; // Assurez-vous que le conteneur des boutons est au-dessus de la vidéo
 
     var closeButton = document.createElement("button");
-closeButton.textContent = "✖";
-closeButton.style.fontSize = "20px";
-closeButton.style.marginRight = "10px"; // Espacement entre les boutons
-closeButton.addEventListener("click", function () {
-    characterDiv.remove(); // Supprimer l'élément contenant l'image du personnage et les boutons
-});
-
+    closeButton.textContent = "✖";
+    closeButton.style.fontSize = "20px";
+    closeButton.style.marginRight = "10px"; // Espacement entre les boutons
+    closeButton.addEventListener("click", function () {
+        characterDiv.remove(); // Supprimer l'élément contenant l'image du personnage et les boutons
+    });
 
     var saveButton = document.createElement("button");
-saveButton.textContent = "Save";
-saveButton.style.fontSize = "20px";
-saveButton.style.marginLeft = "10px"; // Espacement entre les boutons
-saveButton.addEventListener("click", function () {
-    var characterInDropZone = dropZoneDiv.querySelector("img");
-    if (characterInDropZone) {
-        var characterPath = characterInDropZone.src;
-        var url = new URL(characterPath);
-        var relativePath = url.pathname; // Obtenez le chemin relatif à partir de l'URL
-        if (relativePath.charAt(0) === '/') {
-            relativePath = relativePath.substring(1); // Supprimer le premier caractère s'il est "/"
+    saveButton.textContent = "Save";
+    saveButton.style.fontSize = "20px";
+    saveButton.style.marginLeft = "10px"; // Espacement entre les boutons
+    saveButton.addEventListener("click", function () {
+        var characterInDropZone = dropZoneDiv.querySelector("img");
+        if (characterInDropZone) {
+            var characterPath = characterInDropZone.src;
+            var url = new URL(characterPath);
+            var relativePath = url.pathname; // Obtenez le chemin relatif à partir de l'URL
+            if (relativePath.charAt(0) === '/') {
+                relativePath = relativePath.substring(1); // Supprimer le premier caractère s'il est "/"
+            }
+            alert("Character saved : \"" + relativePath + "\"");
+        } else {
+            alert("No character selected.");
         }
-        alert("Character saved : \"" + relativePath + "\"");
-
-    } else {
-        alert("No character selected.");
-    }
-});
+    });
 
     buttonsContainerDiv.appendChild(closeButton);
     buttonsContainerDiv.appendChild(saveButton);
@@ -117,5 +134,3 @@ saveButton.addEventListener("click", function () {
 
     document.body.appendChild(characterDiv);
 }
-
-
